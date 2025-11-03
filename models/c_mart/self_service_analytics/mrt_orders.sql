1821eac49681dcac277fb352d19edd6b20d3cbc8
Yoann B <yb.blanchet@hotmail.com>
2025-11-03 13:35:27 +0000

    feat: update mrt

--- models/c_mart/self_service_analytics/mrt_orders.sql
+++ models/c_mart/self_service_analytics/mrt_orders.sql
@@ -1,7 +1,15 @@
+{{ config(
+    partition_by= {
+      "field": "order_date",
+      "data_type": "date"
+    }
+)}}
+
 with cte_main as (
     select
         *
     from {{ ref('int_sales__orders') }}
+    order by order_id
 )
 
 select 
--- models/c_mart/self_service_analytics/mrt_products.sql
+++ models/c_mart/self_service_analytics/mrt_products.sql
@@ -2,6 +2,7 @@ with cte_main as (
     select
         *
     from {{ ref('int_production__products') }}
+    order by product_id
 )
 
 select 
