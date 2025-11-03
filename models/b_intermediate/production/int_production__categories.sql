214529c3e3ec337d074cd5583b826d824ddd27a4
Yoann B <yb.blanchet@hotmail.com>
2025-11-03 12:45:15 +0000

    fix: fix lineage use

--- models/b_intermediate/production/int_production__categories.sql
+++ models/b_intermediate/production/int_production__categories.sql
@@ -1,18 +1,9 @@
 with cte_main as(
     select
-        c.category_id as category_id, 
-        c.category_name as category_name,
-        sum(quantity_stock) as quantity_stock,
-        sum(quantity_sold) as quantity_sold,
-        round(sum(total_item_amount),2) as total_item_amount
-    from 
-        {{ ref('stg_production__categories') }} as c
-    left join
-        {{ ref('int_production__products') }} as p
-    using(category_id)
-    group by
-        category_id,
+        category_id, 
         category_name
+    from 
+        {{ ref('stg_production__categories') }}
 )
 
 select 
