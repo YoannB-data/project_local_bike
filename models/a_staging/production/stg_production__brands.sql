4ea887f02b65c1d12974ea8bb00133c1bee9377b
Yoann B <yb.blanchet@hotmail.com>
2025-11-04 07:28:55 +0000

    fix: fix sql syntaxe

--- models/a_staging/production/stg_production__brands.sql
+++ models/a_staging/production/stg_production__brands.sql
@@ -1,4 +1,5 @@
 select 
     cast(brand_id as int64) as brand_id, 
     cast(brand_name as string) as brand_name
+    
 from {{ source("database", "brands") }}
--- models/a_staging/production/stg_production__categories.sql
+++ models/a_staging/production/stg_production__categories.sql
@@ -1,4 +1,5 @@
 select
     cast(category_id as int64) as category_id,
     cast(category_name as string) as category_name
+    
 from {{ source("database", "categories") }}
